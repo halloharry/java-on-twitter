3115a1be76b8d39f1cddb47ccf53d6da14d1f131
harryrosadi <harry.harry.rosadi@gmail.com>
2023-08-25 18:46:10 +0700

    contoh array dan Arraylist

---
+++ src/twitter/arraydanlist/ContohArrayDanArrayList.java
@@ -0,0 +1,30 @@
+package twitter.arraydanlist;
+
+import java.util.ArrayList;
+
+public class ContohArrayDanArrayList {
+    public static void main(String[] args) {
+        // Menggunakan array untuk menyimpan nilai-nilai ujian
+        int[] nilaiUjian = {85, 90, 78, 92, 88};
+
+        // Menampilkan nilai ujian dari array
+        System.out.print("Nilai Ujian dari Array: ");
+        for (int i = 0; i < nilaiUjian.length; i++) {
+            System.out.print(nilaiUjian[i] + " ");
+        }
+        System.out.println();
+
+        // Menggunakan ArrayList untuk menyimpan daftar buah
+        ArrayList<String> daftarBuah = new ArrayList<>();
+        daftarBuah.add("Apel");
+        daftarBuah.add("Jeruk");
+        daftarBuah.add("Mangga");
+
+        // Menampilkan daftar buah dari ArrayList
+        System.out.print("Daftar Buah dari ArrayList: ");
+        for (String buah : daftarBuah) {
+            System.out.print(buah + " ");
+        }
+    }
+}
+
---
+++ src/twitter/arraydanlist/ContohArrayList.java
@@ -0,0 +1,25 @@
+package twitter.arraydanlist;
+
+import java.util.ArrayList;
+
+public class ContohArrayList {
+    public static void main(String[] args) {
+        // Membuat ArrayList untuk menyimpan nama-nama buah
+        ArrayList<String> daftarBuah = new ArrayList<>();
+
+        // Menambahkan buah ke dalam ArrayList
+        daftarBuah.add("Apel");
+        daftarBuah.add("Jeruk");
+        daftarBuah.add("Mangga");
+
+        // Menampilkan isi ArrayList
+        System.out.println("Daftar Buah: " + daftarBuah);
+
+        // Menghapus buah dari ArrayList
+        daftarBuah.remove("Jeruk");
+
+        // Menampilkan isi ArrayList setelah penghapusan
+        System.out.println("Daftar Buah setelah menghapus Jeruk: " + daftarBuah);
+    }
+}
+
